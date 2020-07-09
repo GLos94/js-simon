0b2771164c5f29a85dc5dc2fd704deb2defdf9e7
Giulia <giulialosito94@gmail.com>
2020-07-09 16:48:46 +0200

    Esercizio Simon completo

--- js/script.js
+++ js/script.js
@@ -1,52 +1,50 @@
+// Un alert espone 5 numeri casuali (univoci).
 
-
-
-
-$(document).ready(function functionName() {
-
-  var numCasuali = 0;
+$(document).ready(function() {
   var arrayNumCasuali = [];
   var arrayNumUtente = [];
-// Un alert espone 5 numeri casuali (univoci).
-  for (var i = 0; i < 5; i++) {
-  numCasuali = alert(getRandomIntInclusive(1,100));
-  console.log(numCasuali);
-  arrayNumCasuali.push(numCasuali);
-}
-console.log(arrayNumCasuali);
+
+  $('#genera').click(function() {
+    for (var i = 0; i < 5; i++) {
+    var numCasuali = $('#random').val;
+    numCasuali = getRandomIntInclusive(1,100);
+    alert(numCasuali);
+    arrayNumCasuali.push(numCasuali);
+  }
+  console.log('I numeri casuali sono: ' + arrayNumCasuali);
+
+  })
+
+
 
 // Poi parte un timer di 30 secondi.
 var timer = setTimeout(function() {
-  console.log(timer);
-  console.log('hello');
-
-
-},3000*10);
-
 
 // Dopo 30 secondi l’utente deve inserire, un prompt alla volta, i numeri che ha visto precedentemente.
-for (var i = 0; i < 5; i++) {
-  var numUtente = parseInt(prompt('Inserisci i numeri che hai visto' ));
-  arrayNumUtente.push(numUtente);
+  for (var i = 0; i < 5; i++) {
+    var numUtente = parseInt(prompt('Inserisci i numeri che hai visto' ));
+    arrayNumUtente.push(numUtente);
 
-};
+  };
 
-console.log(arrayNumUtente);
+  console.log('I tuoi numeri sono: ' + arrayNumUtente);
 
-});
 
 // Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati
-// var punteggio = 0;
-// var numIndovinati = [];
-// for (var i = 0; i < arrayNumUtente.length; i++) {
-//   if (checkCompare(arrayNumCasuali,arrayNumUtente[i])) {
-//     punteggio++
-//     indovinati.push(arrayNumUtente[i])
-//   }
-//
-// }
+var punteggio = 0;
+  for (var i = 0; i < arrayNumUtente.length; i++) {
+    if (checkCompare(arrayNumCasuali, arrayNumUtente[i])) {
+    punteggio++
+  };
+};
+
+console.log('Hai totalizzato ' + punteggio + ' punti');
+
+},3000);
 
 
+
+});
 
 // functions
 function getRandomIntInclusive(min, max) {
@@ -55,7 +53,7 @@ function getRandomIntInclusive(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function checkCompare (array, num ){
+function checkCompare(array, num ){
   var key = false;
   for (var i = 0; i < array.length; i++) {
    if (array[i]== num) {
